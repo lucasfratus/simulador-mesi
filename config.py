20093515f85bb6dbef7ccbfa6092df8da7a22ba7
LorenzoHz <zlorenzoh@gmail.com>
2025-01-28 22:15:16 -0300

    Update instruction set and configuration parameters; change processor count to 2 and replacement policy to LFU

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Description: Arquivo de configuração do simulador MESI
 CONFIGURACOES = {
-    "n_processadores": 4,
+    "n_processadores": 2,
     "tamanho_linha": 2,
     "n_linhas_cache_compartilhada": 16,
     "n_linhas_cache_privada": 4,
     "n_linhas_conjunto": 2,
-    "politica_substituicao": 'FIFO'
+    "politica_substituicao": 'LFU'
 }
